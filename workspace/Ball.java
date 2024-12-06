43e4bfb0348cf770c811ad144f3c131c1cca2968
ChristianM <106447493+MRC13Games@users.noreply.github.com>
2024-12-06 13:55:16 +0000

    commit -m “Christian Murillo: Submitted on 12/6/2024”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,99 @@
 import java.awt.*;
 
+//
+//  Class author:  Christian M
+//  Date created:  12/4/2024
+//  General description: this makes a ball for the game that bounces around and destroys blocks
+//
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int Xpos;
+	private int Ypos;
+	private int size;
+	private int XVelocity;
+	private int YVelocity;
+	private int reverseX;
+	private int reverseY;
 	
 	//constructor(s):
-	
-	//methods:
+	public Ball(int initXpos, int initYpos, int initSize){
+		Xpos = initXpos;
+		Ypos = initYpos;
+		size = initSize;
+		XVelocity = 2;
+		YVelocity = 2;
+	}
 
+	//methods:
+	public void draw(Graphics g){
+		g.setColor(Color.green);
+		g.fillOval(Xpos , Ypos, size, size);
+	}
+
+
+	public void setSize(int size){
+		this.size = size;
+	}
+	public int getSize(){
+		return size;
+	}
+
+
+	public void setX(int Xpos){
+		this.Xpos = Xpos;
+	}
+	public int getXpos(){
+		return Xpos;
+	}
+
+
+	public void setY(int Ypos){
+		Ypos = this.Ypos;
+	}
+	public int getYpos(){
+		return Ypos;
+	}
+
+
+	public void setYVelocity(int YVelocity){
+		this.YVelocity = YVelocity;
+	}
+	public int getYVelocity(){
+		return YVelocity;
+	}
+
+
+	public void setXVelocity(int XVelocity){
+		this.XVelocity = XVelocity;
+	}
+	public int getXVelocity(){
+		return XVelocity;
+	}
+
+//
+//  Pre-condition: reverse x and y must input an int valuse
+//  Post-condition: will reverse that velocity
+//
+
+	public int reverseX(){
+		XVelocity = -XVelocity;
+		return reverseX;
+	}
+	public int reverseY(){
+		YVelocity = -YVelocity;
+		return reverseY;
+	}
+
+//
+//  Pre-condition: move must input a void
+//  Post-condition: will move the ball
+//
+
+	public void move(){
+		Xpos += XVelocity;
+		Ypos += YVelocity;
+	}
+	
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,4 +1,9 @@
-import java.awt.Graphics;
+//
+//  Class author:  Christian M
+//  Date created:  12/4/2024
+//  General description: this makes a paddle for the game, moves left and right and bounces the ball
+//
+
 import java.awt.*;
 
 public class Paddle {
@@ -6,11 +11,79 @@ public class Paddle {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int X;
+	private int Y;
+	private int Width;
+	private int Height;
+	private	int Velocity;
+
 	
 	//constructor(s):
-	
+	public Paddle(int initX, int initY, int initWidth, int initHeight){
+		X = initX;
+		Y = initY;
+		Width = initWidth;
+		Height = initHeight;
+	}
+
 	//methods:
+	public void draw(Graphics g){
+		g.setColor(Color.yellow);
+		g.fillRect(X , Y, Width, Height);
+	}
+
+	public void setX(int X){
+		this.X = X;
+	}
+	public int getX(){
+		return X;
+	}
+
+	public void setY(int Y){
+		this.Y = Y;
+	}
+	public int getY(){
+		return Y;
+	}
+
 	
-	
+	public void setWidth(int Width){
+		this.Width = Width;
+	}
+	public int getWidth(){
+		return Width;
+	}
+
+	public void setHeight(int Height){
+		this.Height = Height;
+	}
+	public int getHeight(){
+		return Height;
+	}
+
+	public void setVelocity(int Velocity){
+		this.Velocity = Velocity;
+	}
+	public int getVelocity(){
+		return Velocity;
+	}
+
+	public int addVelocity(int Velocity){
+		this.Velocity += Velocity;
+		return Velocity;
+	}
+
+//
+//  Pre-condition: move must input a void
+//  Post-condition: will move the paddle left or right
+//
+	public void move(){
+		X += Velocity;
+		if(Velocity > 0){
+			Velocity--;
+		}
+		if (Velocity<0){
+			Velocity++;
+		}
+	}
 }
